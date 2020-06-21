2dac1529edfa9744dd916f2920d67f5212c31ed1
Eshan Vohra <eshanvohra@gmail.com>
2020-06-21 17:59:48 +0530

    21june

--- Dance website/app.js
+++ Dance website/app.js
@@ -12,10 +12,16 @@ app.set('view engine', 'pug') // Set the template engine as pug
 app.set('views', path.join(__dirname, 'views')) // Set the views directory
  
 // ENDPOINTS
-app.get('/', (req, res)=>{
+app.get('/home', (req, res)=>{
 
     const params = {}
-    res.status(200).render('index.pug', params);
+    res.status(200).render('home.pug', params);
+})
+
+app.get('/contact', (req, res)=>{
+
+    const params = {}
+    res.status(200).render('contact.pug', params);
 })
 
 
