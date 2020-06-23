8274e923ad0c2d51f14718e5922b92604c1b7b63
Eshan Vohra <eshanvohra@gmail.com>
2020-06-23 17:55:55 +0530

    23june

--- Dance website/app.js
+++ Dance website/app.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const path = require('path');
+const bodyparser = require("body-parser");
 const app = express();
 const port = 8003;
+
+// getting-started.js
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true ,useUnifiedTopology: true } );
+
+
+
+//define mongoose schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+  });
+
+  const Contact = mongoose.model('Contact', contactSchema);
+
+
  
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
-app.use(express.urlencoded())
+app.use(express.urlencoded());
 
 // PUG SPECIFIC STUFF
 app.set('view engine', 'pug') // Set the template engine as pug
@@ -22,6 +42,17 @@ app.get('/contact', (req, res)=>{
 
     const params = {}
     res.status(200).render('contact.pug', params);
+})
+
+app.post('/contact', (req, res)=>{
+
+    var myData=new Contact(req.body);
+    myData.save().then(()=>{
+        res.send("This item has been saved")
+    }).catch(()=>{
+        res.status(400).send("Item was not saved to db")
+    })
+    // res.status(200).render('contact.pug');
 })
 
 
